46fe2c3c31267727a394f89bd312b1fa6bff34e2
fray osorio <frayosorio@gmail.com>
2025-05-19 15:32:23 -0500

    Se agregan las entidades SELECCION y CAMPEONATO

---
+++ src/main/java/campeonatosfifa/api/core/dominio/entidades/Campeonato.java
@@ -0,0 +1,71 @@
+package campeonatosfifa.api.core.dominio.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "campeonato")
+public class Campeonato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "secuencia_campeonato")
+    @SequenceGenerator(name = "secuencia_campeonato", sequenceName = "secuencia_campeonato", allocationSize = 1)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "campeonato", unique = true, length = 100)
+    private String nombre;
+    @Column(name = "año")
+    private int año;
+
+    @JoinColumn(name = "idpais", referencedColumnName = "id")
+    private Seleccion paisOrganizador;
+
+    public Campeonato(int id, String nombre, int año, Seleccion paisOrganizador) {
+        this.id = id;
+        this.nombre = nombre;
+        this.año = año;
+        this.paisOrganizador = paisOrganizador;
+    }
+
+    public Campeonato() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public Seleccion getPaisOrganizador() {
+        return paisOrganizador;
+    }
+
+    public void setPaisOrganizador(Seleccion paisOrganizador) {
+        this.paisOrganizador = paisOrganizador;
+    }
+
+}
---
+++ src/main/java/campeonatosfifa/api/core/dominio/entidades/Seleccion.java
@@ -0,0 +1,58 @@
+package campeonatosfifa.api.core.dominio.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pais")
+public class Seleccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "secuencia_pais")
+    @SequenceGenerator(name = "secuencia_campeonato", sequenceName = "secuencia_campeonato", allocationSize = 1)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "pais", unique = true, length = 100)
+    private String nombre;
+    @Column(name = "entidad")
+    private String entidad;
+
+    public Seleccion() {
+    }
+
+    public Seleccion(int id, String nombre, String entidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.entidad = entidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEntidad() {
+        return entidad;
+    }
+
+    public void setEntidad(String entidad) {
+        this.entidad = entidad;
+    }
+
+}
