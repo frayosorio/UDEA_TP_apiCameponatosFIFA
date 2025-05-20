ac4a29c830783019d96dd5b8f112c86abd34f5df
fray osorio <frayosorio@gmail.com>
2025-05-20 12:17:14 -0500

    Se agregan todas las entidades del dominio

---
+++ src/main/java/campeonatosfifa/api/core/dominio/entidades/Ciudad.java
@@ -0,0 +1,63 @@
+package campeonatosfifa.api.core.dominio.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ciudad")
+public class Ciudad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "secuencia_ciudad")
+    @SequenceGenerator(name = "secuencia_ciudad", sequenceName = "secuencia_ciudad", allocationSize = 1)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "ciudad", nullable = false)
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "idpais", referencedColumnName = "id", nullable = false)
+    private Seleccion pais;
+
+    public Ciudad() {
+    }
+
+    public Ciudad(int id, String nombre, Seleccion pais) {
+        this.id = id;
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Seleccion getPais() {
+        return pais;
+    }
+
+    public void setPais(Seleccion pais) {
+        this.pais = pais;
+    }
+}
+
